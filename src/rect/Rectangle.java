eb8a2a0e44e29a163ac240354e62df4b8f87b247
Theerapat Boonrod <xsw12560@gmail.com>
2024-08-17 03:17:00 +0700

    Set width/height to 1 if given width/height < 1

--- src/rect/Rectangle.java
+++ src/rect/Rectangle.java
@@ -5,8 +5,8 @@ public class Rectangle {
 	private int width, height;
 	public Rectangle(int width, int height) {
 		super();
-		this.width =width;
-		this.height = height;
+		this.width =Math.max(width, 1);
+		this.height = Math.max(1, height);
 	}
 	public int getWidth() {
 		return width;
